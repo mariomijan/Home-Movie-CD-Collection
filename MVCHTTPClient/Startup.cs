f7401d8e249004f2b27448e43584a995a8f5822c
mariomijan <mijanmario@gmail.com>
2018-01-25 21:05:18 +0100

    Add files via upload

---
+++ MVCHTTPClient/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCHTTPClient.Startup))]
+namespace MVCHTTPClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
